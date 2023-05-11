c56732f668472c9bb19b0b807e7183f7b3fe57ab
Pastafredda <justspotify09@gmail.com>
2023-05-12 01:52:33 +0200

    quasi

--- script.js
+++ script.js
@@ -1,50 +1,82 @@
 // Cliccando su play facciamo diventare visibile la griglia
 const play =document.getElementById("play");
 const grid = document.querySelector(".grid");
+let score= 0;
 
-play.addEventListener('click',
-    function(){
+play.addEventListener('click', startGame)
+function startGame(){
 
-        // Facciamo apparire la griglia
-        grid.classList.add('active');
+    const arrayNumeri= randomNumber(16, 1, 100);
+    console.log(arrayNumeri);
 
-        // Ripuliamo la griglia quando clicchiamo su play
-        grid.innerHTML= '';
 
-        // Creiamo 100 square
-        for (let i = 1; i < 100 + 1; i++){
-            
-            // Tramite la funzione andiamo a creare i due elementi
-            const newSquare = createGridSquare("div", "square");
+    // Facciamo apparire la griglia
+    grid.classList.add('active');
 
-            // Facciamo apparire i numeri all'interno degli square
-            const span = document.createElement("span")
-            span.append(i);
-            newSquare.append(span);
+    // Ripuliamo la griglia quando clicchiamo su play
+    grid.innerHTML= '';
+
+    // Creiamo 100 square
+    for (let i = 1; i < 100 + 1; i++){
         
-            // Al click coloriamo lo square
-            newSquare.addEventListener("click",
-                function () {
-                    newSquare.classList.add("square-green");
-                    console.log(i);
+        // Tramite la funzione andiamo a creare i due elementi
+        const newSquare = createGridSquare("div", "square");
+
+        // Al click coloriamo lo square
+        newSquare.addEventListener("click",
+            function () {
+                newSquare.classList.add("square-green");
+                console.log(i);
+                
+                // Facciamo apparire i numeri all'interno degli square
+                const span = document.createElement("span")
+                span.append([i]);
+                newSquare.append(span);
+
+                // 
+                if(arrayNumeri.includes(i)){
+                    newSquare.classList.add("square-red");
+                    console.log("bravo hai totalizzato", score, "punti");
+                }else{
+                    ++score
+                    console.log("il tuo punteggio è", score);
                 }
-            )
-
-            grid.append(newSquare);
-    
-        }
-        
-        // Creiamo gli elementi per inserirli nella griglia
-        
+            }
+        )
+        grid.append(newSquare);
     }
-)
+}
 
+// Funzioni
+
+// Creiamo gli elementi per inserirli nella griglia
 function createGridSquare(tagType, classToAdd) {
     const newElement = document.createElement(tagType);
     newElement.classList.add(classToAdd);
     return newElement;
     
 }
+
+// Creiamo l'array di numeri casuali in un range 
+function randomNumber(numeroBombe,min, max){
+    const arrayCasuale= [];
+    // Finchè non raggiungi la fine dell'array
+    while(arrayCasuale.length < numeroBombe){ 
+        // Crei un numero casuale 
+        const nuovoNumero= randomMinMax(min, max);
+        // Se nell'array non è incluso un nuovo numero
+        if(!arrayCasuale.includes(nuovoNumero)){
+            // Allora lo pushiamo cosi evitiamo di pushare due o più numeri uguali
+            arrayCasuale.push(nuovoNumero);
+        }
+    }
+    return arrayCasuale;
+}
+
+function randomMinMax (min, max){
+    return Math.floor(Math.random()* ( max - min + 1)) + min;
+}
+
 
 
 
